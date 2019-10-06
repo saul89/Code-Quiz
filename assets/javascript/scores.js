7d024a1d3493527b0f359684f06f86b7eabcf0bc
SK26 <saul.lazzarini@gmail.com>
2019-10-06 22:14:19 +0200

    Code Quiz 100% working

--- assets/javascript/scores.js
+++ assets/javascript/scores.js
@@ -1,5 +1,8 @@
 var clearHighscores = document.querySelector("#clearhighscores"),
-olEL = document.createElement("ol");
+olEL = document.createElement("ol"),
+scoresArray = [],
+scoresArrayDesc = [],
+scoresArrayLi = [];
 
 clearHighscores.addEventListener("click",function(event){
     event.preventDefault();
@@ -11,8 +14,30 @@ olEL.setAttribute("id","olScoresEl");
 document.body.querySelector(".main-content").insertBefore(olEL, document.querySelector(".main-content").children[1]);
 
 for(var i = 0 ; i < localStorage.length ; i++ ){
+    var score = localStorage.key(i) + " - " + localStorage.getItem(localStorage.key(i));
+    var scoreDesc = localStorage.getItem(localStorage.key(i));
+    scoresArray.push(score);
+    scoresArrayDesc.push(scoreDesc);
+  }
+
+  console.log(scoresArray);
+
+  scoresArrayDesc.sort(function(a, b){return b-a});
+
+  console.log(scoresArrayDesc);
+
+  for(var i=0 ;  i < scoresArray.length ; i++ ){
+    for(var j = 0 ;  j < scoresArrayDesc.length ; j++){
+    if(scoresArray[i].slice(5).includes(scoresArrayDesc[j])){
+      scoresArrayLi[j] = scoresArray[i];
+    };
+    }
+  }
+  
+  for(var i = 0 ; i < scoresArrayLi.length ; i++ ){
     var tagLi = document.createElement("li");
     tagLi.setAttribute("id", "liScoreEl");
-    tagLi.textContent = localStorage.key(i)+ " - " +localStorage.getItem(localStorage.key(i));
+    tagLi.textContent = scoresArrayLi[i];
     document.querySelector("#olScoresEl").appendChild(tagLi);
   }
+ 
